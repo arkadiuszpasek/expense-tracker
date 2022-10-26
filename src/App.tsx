b34e6031e955c4f5eca9ad3eb51a8f9b8cb0f22b
Arkadiusz Pasek <arkadiusz.pasek99@gmail.com>
2022-10-26 21:12:56 +0200

    Add app with embeded form

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfRlHkkkfi2P4Nap2a0Xp0ZWfm2eZqLIuM8445O_oWsmrwpDg/viewform?embedded=true" width="100%" height="701" frameBorder="0" marginHeight={0} marginWidth={0}>Loading…</iframe>
+    </div>
+  );
+}
+
+export default App;
